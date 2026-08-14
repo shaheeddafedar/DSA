7a7d1d0388b1fda651584bf5ea279b12e482a63e
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-14 23:48:39 +0530

    First Pattern

---
+++ Pattern_Matching/First_Pattern.java
@@ -0,0 +1,12 @@
+package Pattern_Matching;
+
+public class First_Pattern {
+    public static void main(String[] args) {
+        for (int i = 0; i <4; i++) {
+            for (int j = 0; j <4; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
