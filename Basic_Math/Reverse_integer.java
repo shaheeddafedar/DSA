c6a9b68d449edc3095949f600283cfd1df1bfb10
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-17 21:21:45 +0530

    Reverse integer

---
+++ Basic_Math/Reverse_integer.java
@@ -0,0 +1,27 @@
+// You are given an integer n. Return the integer formed by placing the digits of n in reverse order.
+
+package Basic_Math;
+
+import java.util.Scanner;
+
+public class Reverse_integer {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Integer values : ");
+        int n = input.nextInt();
+        int result = reverse(n);
+        System.out.println("Reversed value  : " + result);
+        input.close();
+        
+    }
+    public static int reverse(int n){
+        int digit=0;
+        int revernum =0;
+        while (n!=0) {
+            digit=n%10;
+            revernum=revernum*10+digit;
+            n=n/10;
+        }
+        return revernum;
+    }
+}
