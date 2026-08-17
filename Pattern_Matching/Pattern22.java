7a22dd279cbce1c9fce57cef5eb6ef8ca5d30947
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-17 20:23:18 +0530

    Pattern

---
+++ Pattern_Matching/Pattern22.java
@@ -0,0 +1,21 @@
+package Pattern_Matching;
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern22(n);
+    }
+
+    public static void pattern22(int n) {
+        for (int i = 0; i < 2 * n - 1; i++) {
+            for (int j = 0; j < 2 * n - 1; j++) {
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int down = (2 * n - 2) - i;
+                System.out.print(n - Math.min(Math.min(top, down), Math.min(left, right)));
+            }
+            System.out.println();
+        }
+    }
+}
