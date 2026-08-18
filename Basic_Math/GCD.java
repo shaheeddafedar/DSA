7e99cac655ed3f7c7ce05f473c29b45ca2a21997
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-18 19:08:42 +0530

    check for GCD

--- Basic_Math/GCD.java
+++ Basic_Math/GCD.java
@@ -28,3 +28,15 @@ public class GCD {
         return max;
     }
 }
+
+
+// GCD Using elucide algorithm
+// public static int GCD(int n1, int n2) {
+//     while (n2 != 0) {
+//         int temp = n2;
+//         n2 = n1 % n2;
+//         n1 = temp;
+//     }
+
+//     return n1;
+// }
