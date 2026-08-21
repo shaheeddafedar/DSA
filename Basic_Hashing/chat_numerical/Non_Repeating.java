90c2b32df1d99ccf0b5b906fc7e743e069673b4f
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-21 23:21:51 +0530

    Print n to 1  number using recursion

--- Basic_Hashing/chat_numerical/Non_Repeating.java
+++ Basic_Hashing/chat_numerical/Non_Repeating.java
@@ -7,20 +7,23 @@ import java.util.HashMap;
 public class Non_Repeating {
     public static void main(String[] args) {
         int [] arr = {1, 2, 2, 3, 1, 4};
-        non_repeat(arr);
+        int result =non_repeat(arr);
+        System.out.println(result);
         
     }
-    public static void non_repeat(int []arr){
+    public static int non_repeat(int []arr){
         HashMap<Integer,Integer> hm = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
         }
-        for (Integer key : hm.keySet()) {
-          int frequency = hm.get(key);
-          if (frequency==1) {
-            System.out.println(key);
+        int unique=0;
+    for (int i = 0; i <arr.length; i++) {
+        if (hm.get(arr[i])==1) {
+            unique=arr[i];
             break;
-          }
         }
+    }
+        
+        return unique;
     }
 }
---
+++ Basic_Hashing/chat_numerical/uniqueinarray.java
@@ -0,0 +1,30 @@
+// Check If Two Arrays Have Common Elements
+package Basic_Hashing.chat_numerical;
+
+import java.util.HashSet;
+
+public class uniqueinarray {
+    public static void main(String[] args) {
+        int []arr1 ={1, 2, 3, 4};
+        int []arr2={7, 8, 3, 9};
+        if (check(arr1, arr2)) {
+            System.out.println("true");
+        } else{
+            System.out.println("False");
+        }
+        
+        
+    }
+    public static boolean check(int []arr1,int []arr2){
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < arr1.length; i++) {
+            hs.add(arr1[i]);
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            if (hs.contains(arr2[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
