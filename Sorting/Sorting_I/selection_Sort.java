e64814239c1af504848534e3b88ecea48caa7c0d
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-22 23:08:01 +0530

    selection sort

---
+++ Sorting/Sorting_I/selection_Sort.java
@@ -0,0 +1,30 @@
+package Sorting.Sorting_I;
+
+public class selection_Sort {
+    public static void main(String[] args) {
+        int [] arr={13,46,24,52,20,9};
+        int [] reuslt = selectionSort(arr);
+        for (int i = 0; i < reuslt.length; i++) {
+            System.out.println(+reuslt[i]);
+        }
+    }
+
+    public static int[] selectionSort(int[] nums) {
+
+        for (int i = 0; i <nums.length-1; i++) {
+            int mini =i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j]<nums[mini]) {
+                mini=j;
+                }
+            }
+            swap(nums, mini,i);
+        }
+        return nums;
+    }
+    public static void swap( int arr[],int mini,int i){
+        int temp = arr[mini];
+        arr[mini]=arr[i];
+        arr[i]=temp;
+    } 
+}
