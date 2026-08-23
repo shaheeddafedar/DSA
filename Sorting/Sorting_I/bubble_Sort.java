b3272f70a69442485f8727f44905d820447066f0
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-23 18:47:00 +0530

    Bubble sort

--- Sorting/Sorting_I/bubble_Sort.java
+++ Sorting/Sorting_I/bubble_Sort.java
@@ -3,20 +3,24 @@ package Sorting.Sorting_I;
 public class bubble_Sort {
     public static void main(String[] args) {
         int [] arr ={13,24,52,20,9};
+        int []result = bubbleSort(arr);
+      for (int i = 0; i < result.length; i++) {
+        System.out.println(result[i]);
+      }
     }
         public static int[] bubbleSort(int[] nums) {
             for (int i = 0; i < nums.length-1; i++) {
-                for (int j = i+1; j < nums.length; j++) {
-                    if (nums[j]<nums[i]) {
-                        nums[j]=nums[i];
-                    }
-                    swap(nums, i, j);
-                }
+              for (int j = 0; j < nums.length-i-1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                swap(nums, j);
+            }
+        }
             }    
+            return nums; 
     }
-    public static void swap(int []nums,int i,int j){
-        int temp = nums[i];
-        nums[i]=nums[j];
-        nums[j]=nums[i];
+    public static void swap(int []nums,int i){
+        int temp = nums[i+1];
+        nums[i+1]=nums[i];
+        nums[i]=temp;
     }
 }
