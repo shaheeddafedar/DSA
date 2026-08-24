bb1d41da268854260ab4c5e560e315d9a93c54c0
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-08-25 00:28:43 +0530

    Insertion_Sort uing Ascending and Descending

--- Sorting/Sorting_I/insertion_Sort.java
+++ Sorting/Sorting_I/insertion_Sort.java
@@ -2,20 +2,23 @@ package Sorting.Sorting_I;
 
 public class insertion_Sort {
     public static void main(String[] args) {
-        int [] nums ={5,4,3,2,1};
-        // int [] result1=insertion_Sort_Ass(nums);
-        // for (int i = 0; i < result1.length; i++) {
-        //     System.out.println(result1[i]);
-        // }
-                int [] result2=insertion_Sort_Dec(nums);
-        for (int i = result2.length; i >1; i--) {
+        int[] nums = { 5, 4, 3, 2, 1 };
+        System.out.println("Insertion Sort using Ascending  Order");
+        int [] result1=insertion_Sort_Ass(nums);
+        for (int i = 0; i < result1.length; i++) {
+        System.out.println(result1[i]);
+        }
+                System.out.println("Insertion Sort using Descending  Order");
+        int[] result2 = insertion_Sort_Dec(nums);
+        for (int i = 0; i < result2.length; i++) {
             System.out.println(result2[i]);
         }
     }
-    public static int [] insertion_Sort_Ass(int [] nums){
-        for (int i = 1; i <nums.length; i++) {
-            int j =i;
-            while (j>0&&nums[j]<nums[j-1]) {
+
+    public static int[] insertion_Sort_Ass(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int j = i;
+            while (j > 0 && nums[j] < nums[j - 1]) {
                 swap(nums, j);
                 j--;
             }
@@ -23,10 +26,10 @@ public class insertion_Sort {
         return nums;
     }
 
-        public static int [] insertion_Sort_Dec(int [] nums){
-        for (int i = 1; i <nums.length; i++) {
-            int j =i;
-            while (j>0&&nums[j]<nums[j-1]) {
+    public static int[] insertion_Sort_Dec(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int j = i;
+            while (j > 0 && nums[j] > nums[j - 1]) {
                 swap(nums, j);
                 j--;
             }
@@ -34,9 +37,9 @@ public class insertion_Sort {
         return nums;
     }
 
-    public static void swap(int [] arr, int j){
-        int temp = arr[j-1];
-        arr[j-1]=arr[j];
-        arr[j]=temp;
+    public static void swap(int[] arr, int j) {
+        int temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
     }
 }
--- Sorting/Sorting_I/selection_Sort.java
+++ Sorting/Sorting_I/selection_Sort.java
@@ -3,13 +3,21 @@ package Sorting.Sorting_I;
 public class selection_Sort {
     public static void main(String[] args) {
         int [] arr={13,46,24,52,20,9};
-        int [] result = selectionSort(arr);
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(+result[i]);
+
+        System.out.println("Selection Sort using Ascending  Order");
+        int [] result1 = selectionSort_Asc(arr);
+        for (int i = 0; i < result1.length; i++) {
+            System.out.println(+result1[i]);
         }
+                System.out.println("Selction Sort using Descending  Order");
+          int [] result2 = selectionSort_Des(arr);
+        for (int i = 0; i < result2.length; i++) {
+            System.out.println(+result2[i]);
+        }
+        
     }
 
-    public static int[] selectionSort(int[] nums) {
+    public static int[] selectionSort_Asc(int[] nums) {
 
         for (int i = 0; i <nums.length-1; i++) {
             int mini =i;
@@ -22,6 +30,21 @@ public class selection_Sort {
         }
         return nums;
     }
+
+        public static int[] selectionSort_Des(int[] nums) {
+
+        for (int i = 0; i <nums.length-1; i++) {
+            int mini =i;
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j]>nums[mini]) {
+                mini=j;
+                }
+            }
+            swap(nums, mini,i);
+        }
+        return nums;
+    }
+
     public static void swap( int arr[],int mini,int i){
         int temp = arr[mini];
         arr[mini]=arr[i];
