254ff1f784e7bf11a4b8de687cc191fdb8222c92
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-09-10 08:38:41 +0530

    Intersection_Array

--- Array/Easy/Intersection_Array.java
+++ Array/Easy/Intersection_Array.java
@@ -8,7 +8,6 @@ public class Intersection_Array {
         int[] arr2 = { 2, 2 };
         int[] result = intersection(arr1, arr2);
         for (int i = 0; i < result.length; i++) {
-            
             System.out.print(result[i] + " ");     
         }
     }
