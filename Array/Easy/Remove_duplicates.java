253a856ce335d5bec7d0e2dc46e2a0dc748a07f0
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-09-22 23:04:41 +0530

    Remove duplicate

--- Array/Easy/Remove_duplicates.java
+++ Array/Easy/Remove_duplicates.java
@@ -9,6 +9,7 @@ public class Remove_duplicates {
         System.out.println("The Lenght of Unique element is : "+ result);
           for (int k = 0; k < result; k++) {
             System.out.println(arr[k]);
+     
         }
     }
 
