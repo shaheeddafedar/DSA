cd5dbd0dd7895b52ce89474c8f26ab67becb9444
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-09-25 00:21:24 +0530

    maximumProduct

--- DSA_180/Arrays/max_Subarray_or_Kadanes.java
+++ DSA_180/Arrays/max_Subarray_or_Kadanes.java
@@ -4,6 +4,7 @@
 // Output: 15
 
 package DSA_180.Arrays;
+
 public class max_Subarray_or_Kadanes {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 5, -2, 7, -4 };
@@ -11,58 +12,58 @@ public class max_Subarray_or_Kadanes {
         System.out.println("The maxSubarray sum is : " + result);
     }
 
-   public static int maxSubArray(int[] arr) {
-        int max=Integer.MIN_VALUE;
-          int sum=0;
+    public static int maxSubArray(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-           sum+=arr[i];
-           if (sum>max) {
-            max=sum;
-           }
-           if (sum<0) {
-            sum=0;
-           }
+            sum += arr[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
         }
         return max;
     }
 }
-
+// Time: O(n)
+// Space: O(1)
 
 // public static void maxSubArray(int[] nums) {
-//     int sum = 0;
-//     int max = Integer.MIN_VALUE;
+// int sum = 0;
+// int max = Integer.MIN_VALUE;
 
-//     int start = 0;
-//     int end = 0;
-//     int tempStart = 0;
+// int start = 0;
+// int end = 0;
+// int tempStart = 0;
 
-//     for (int i = 0; i < nums.length; i++) {
-//         if (sum == 0) {
-//             tempStart = i;
-//         }
-//         sum += nums[i];
-//         if (sum > max) {
-//             max = sum;
-//             start = tempStart;
-//             end = i;
-//         }
-//         if (sum < 0) {
-//             sum = 0;
-//         }
-//     }
-//     System.out.println("Maximum Sum = " + max);
-
-//     System.out.print("Maximum Subarray = [");
-//     for (int i = start; i <= end; i++) {
-//         System.out.print(nums[i]);
-
-//         if (i < end) {
-//             System.out.print(", ");
-//         }
-//     }
-//     System.out.println("]");
+// for (int i = 0; i < nums.length; i++) {
+// if (sum == 0) {
+// tempStart = i;
 // }
+// sum += nums[i];
+// if (sum > max) {
+// max = sum;
+// start = tempStart;
+// end = i;
+// }
+// if (sum < 0) {
+// sum = 0;
+// }
+// }
+// System.out.println("Maximum Sum = " + max);
 
+// System.out.print("Maximum Subarray = [");
+// for (int i = start; i <= end; i++) {
+// System.out.print(nums[i]);
+
+// if (i < end) {
+// System.out.print(", ");
+// }
+// }
+// System.out.println("]");
+// }
 
 // Brute force
 // public static int maxSubArray(int[] arr) {
@@ -81,21 +82,20 @@ public class max_Subarray_or_Kadanes {
 // Time: O(n^3)
 // Space: O(1)
 
-
 // Better Soultion
 // public static int maxSubArray(int[] arr) {
-//         int max=Integer.MIN_VALUE;
-//         for (int i = 0; i < arr.length; i++) {
-//              int sum=0;
-//            for (int j = i; j < arr.length; j++) {
-//             sum+=arr[j];
-//                     max=Math.max(max, sum);
+// int max=Integer.MIN_VALUE;
+// for (int i = 0; i < arr.length; i++) {
+// int sum=0;
+// for (int j = i; j < arr.length; j++) {
+// sum+=arr[j];
+// max=Math.max(max, sum);
 
-//            }   
+// }
 
-//         }
-//         return max;
-//     }
+// }
+// return max;
+// }
 
-    // Time: O(n^2)
+// Time: O(n^2)
 // Space: O(1)
---
+++ DSA_180/Arrays/maximumProduct.java
@@ -0,0 +1,73 @@
+// Given an integer array nums. Find the subarray with the largest product, and return the product of the elements present in that subarray.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// Example 1:
+// Input: nums = [4, 5, 3, 7, 1, 2]
+// Output: 840
+
+package DSA_180.Arrays;
+
+public class maximumProduct {
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 3, 7, 1, 2 };
+        int result = maxProduct(arr);
+        System.out.println("The maximum Product is : " + result);
+    }
+
+    public static int maxProduct(int[] arr) {
+        int max = arr[0];
+        int maxproduct = arr[0];
+        int minproduct = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                int temp = maxproduct;
+                maxproduct = minproduct;
+                minproduct = temp;
+            }
+            maxproduct = Math.max(arr[i], maxproduct *= arr[i]);
+            minproduct = Math.min(arr[i], minproduct *= arr[i]);
+            max = Math.max(max, maxproduct);
+
+        }
+        return max;
+
+    }
+}
+// Time: O(n)
+// Space: O(1)
+
+// brute force
+// public static int maxProduct(int[] arr) {
+// int max=Integer.MIN_VALUE;
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = i; j < arr.length; j++) {
+// int product =1;
+// for (int k = i; k <=j; k++) {
+// product=product*arr[k];
+// }
+// if (product>max) {
+// max=product;
+// }
+// }
+// }
+// return max;
+// }
+// Time: O(n^3)
+// Space: O(1)
+
+// Better solution
+// public static int maxProduct(int[] arr) {
+// int max=Integer.MIN_VALUE;
+// for (int i = 0; i < arr.length; i++) {
+// int product =1;
+// for (int j = i; j < arr.length; j++) {
+// product=product*arr[j];
+
+// if (product>max) {
+// max=product;
+// }
+// }
+// }
+// return max;
+// }
+// Time: O(n^2)
+// Space: O(1)
