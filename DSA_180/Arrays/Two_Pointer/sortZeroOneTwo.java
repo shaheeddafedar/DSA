b9b4d9bb26e77c05e5a1b0b5cfe3454df5e57ca8
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-09-25 17:15:56 +0530

    sortZeroOneTwoo

--- DSA_180/Arrays/Two_Pointer/sortZeroOneTwo.java
+++ DSA_180/Arrays/Two_Pointer/sortZeroOneTwo.java
@@ -1,40 +1,39 @@
-// Given an array nums consisting of only 0, 1, or 2. Sort the array in non-decreasing order.
-// The sorting must be done in-place, without making a copy of the original array.
-// Input: nums = [1, 0, 2, 1, 0]
-// Output: [0, 0, 1, 1, 2]
+package DSA_180.Arrays.Two_Pointer;
 
-package DSA_AtoZ.Array.medium;
-public class sort_Zero_One_Two {
+
+public class sortZeroOneTwo {
     public static void main(String[] args) {
-        int [] arr= {1, 0, 2, 1, 0};
-        sortZeroOneTwo(arr);   
+         int [] arr= {1, 0, 2, 1, 0};
+        sortZeroOneTwoo(arr);   
+        
     }
-       public static void sortZeroOneTwo(int[] arr) {
+        public static void sortZeroOneTwoo(int[] arr) {
         int low =0,mid=0,high=arr.length-1;
         while (mid<=high) {
             if (arr[mid]==0) {
-                swap(arr,low, mid);
+               swap(arr,low,mid);
                 mid++;
                 low++;
             } else if (arr[mid]==1) {
                 mid++;
             } else{
-                swap(arr,mid,high);
-                high--;
+            swap(arr,mid,high);
+             high--;
             }
         }
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]+" ");
+         System.out.print(arr[i]+" ");   
         }
     }
-   public static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
+    public static void swap(int [] arr,int i,int j){
+        int temp =arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
 // Time → O(n)
 // Space → O(1)
+
 
 //Better Solution
 //  public static void sortZeroOneTwo(int[] arr) {
