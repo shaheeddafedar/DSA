d76adcfde9856f402359ba0ff4049fe953ffc05a
Shaheed Dafedar <shaheeddafedar7866@gmail.com>
2026-09-26 21:10:36 +0530

    Threesum

--- DSA_180/Arrays/Two_Pointer/threeSum.java
+++ DSA_180/Arrays/Two_Pointer/threeSum.java
@@ -2,7 +2,6 @@ package DSA_180.Arrays.Two_Pointer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,27 +17,41 @@ public class threeSum {
 
     public static List<List<Integer>> threeSums(int[] arr) {
        Set<List<Integer>> set = new HashSet<>();
-       for (int i = 0; i < arr.length; i++) {
-        Set <Integer> HashSet = new HashSet<>();
-        for (int j = i+1; j < arr.length; j++) {
-            int third =-(arr[i]+arr[j]);
-            if (HashSet.contains(third)) {
-                List<Integer> temp = Arrays.asList(
-                    arr[i],
-                    arr[j],
-                    third  
+      Arrays.sort(arr);
+      for (int i = 0; i < arr.length; i++) {
+        if (i>0&& arr[i]==arr[i-1]) {
+            continue;
+        }
+        int j = i+1;
+        int k = arr.length-1;
+        while (j<k) {
+            int sum = arr[i]+arr[j]+arr[k];
+            if (sum<0) {
+                j++;
+            } else if (sum>0){
+                k--;
+            } else{
+                List<Integer>temp = Arrays.asList(
+                    arr[i],arr[j],arr[k]
                 );
-                Collections.sort(temp);
+                j++;
+                k--;
+                while (j<k&& arr[j]==arr[j-1] ) {
+                    j++;
+                }
+                 while (j<k&& arr[k]==arr[k+1] ) {
+                    k--;
+                }
                 set.add(temp);
             }
-          HashSet.add(arr[j]);
-
         }
-       }
-       return new ArrayList<>(set);
-        
+      }
+      return  new ArrayList<>(set);
     }
 }
+//     Time O(n log n + ) o(n^2)
+//     space o(no of tuples)
+
 
 // Brute Force
 //  public static List<List<Integer>> threeSums(int[] arr) {
